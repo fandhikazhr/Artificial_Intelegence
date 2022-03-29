93e573a56810c8a19ea639f1a18a9cb20dc36285
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-29 09:53:53 +0700

    Update assisstant.py

--- assisstant.py
+++ assisstant.py
@@ -7,3 +7,13 @@ import random
 import webbrowser
 import smtplib
 import subprocess
+
+print("Starting .......")
+
+engine = pyttsx3.init("sapi5")
+rate = engine.getProperty("rate")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+engine.setProperty("rate", rate-30)
+
+engine.say("Initializing ......")
